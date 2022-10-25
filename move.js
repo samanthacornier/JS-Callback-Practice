836e2b94ae0b720ab797e457fa4c5c9ce985ddf8
samanthacornier <samantha112069@gmail.com>
2022-10-25 17:41:10 -0400

    IV Submission

--- move.js
+++ move.js
@@ -1,12 +1,55 @@
-function move(element) {
-    element.style.position = 'fixed'
+function moveWithArrowKeys(left, bottom){
+    let direction = null;
+    let x = left;
+    let y = bottom;
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+    element.style.left = x + 'px'
+    element.style.bottom = y + 'px'
+    
+    function moveCharacter(){ 
+        if(direction === 'west'){
+            x-=1
+        }
+        if(direction === 'north'){
+            y+=1
+        }
+        if(direction === 'east'){
+            x+=1
+        }
+        if(direction === 'south'){
+            y-=1
+        }
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
     }
-
-    return {
-        to: moveToCoordinates
-    }
-}
+    
+    setInterval(moveCharacter, 1)
+    
+    document.addEventListener('keydown', function(e){
+        if(e.repeat) return;
+    
+        if(e.key === 'ArrowLeft'){
+            direction = 'west'
+        }
+        if(e.key === 'ArrowUp'){
+            direction = 'north'
+        }
+        if(e.key === 'ArrowRight'){
+            direction = 'east'
+        }
+        if(e.key === 'ArrowDown'){
+            direction = 'south'
+        }
+        callback()
+    })
+    
+    document.addEventListener('keyup', function(e){
+        direction = null
+        callback()
+    })
+    
+function moveWithArrowKeys(left, bottom, callback){
+    let direction = null;
+    let x = left;
+    let y = bottom;
+    
